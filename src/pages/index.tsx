42f7508b993e5a4c9c7c9a254a0fe1eb103f01d2
Nakaya <eniehack@outlook.jp>
2025-04-13 19:22:33 +0900

    feat: 経路を表示できた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { MapViewState, DeckProps, LayersList } from '@deck.gl/core';
 import { MapboxOverlay } from '@deck.gl/mapbox';
 import { Map, useControl } from 'react-map-gl/maplibre';
-import { GeoJsonLayer } from '@deck.gl/layers';
-import type {Feature, Geometry, FeatureCollection, Point} from 'geojson';
+import { GeoJsonLayer, PathLayer } from '@deck.gl/layers';
+import type {Feature, Geometry, FeatureCollection, Point, LineString} from 'geojson';
 import 'maplibre-gl/dist/maplibre-gl.css'
 import { useEffect, useMemo, useState } from "react";
 
@@ -28,55 +28,87 @@ const DeckGLOverlay = (props: DeckProps) => {
   overlay.setProps(props);
   return null
 }
-type GeoJsonProp = {
-  name: string
+
+type StopGeoJsonProp = {
+  stop_id: string;
+  stop_name: string;
+  route_ids: string[]
 }
 
 export default function Home() {
-  const [stations, setStations] = useState<null | FeatureCollection<Point, GeoJsonProp>>(null);
+  const [stops, setStops] = useState<null | FeatureCollection<Point, StopGeoJsonProp>>(null);
+  const [routes, setRoutes] = useState<null | FeatureCollection<LineString, {shape_id: string}>>(null);
 
   const charset = useMemo<string[]>(() => {
-    if (stations === null) return [];
-    const chars = stations.features
-      .map((d) => Array.from(d.properties.name)) //ラベル文字列を収集
+    if (stops === null) return [];
+    const chars = stops.features
+      .map((d) => Array.from(d.properties.stop_name)) //ラベル文字列を収集
       .flat();
 
     return Array.from(new Set(chars));
-  }, [stations])
+  }, [stops])
 
   const layers = useMemo<LayersList>(() => {
     const layerArr = []
-    if (stations === null) {
+    if (stops === null || routes === null) {
       return []
     }
-    layerArr.push(new GeoJsonLayer<GeoJsonProp>({
+
+    layerArr.push(new GeoJsonLayer<{shape_id: string}>({
+      id: 'routes',
+      data: routes,
+      getText: (d) => d.properties.shape_id,
+      getTextColor: [0xff, 0xff, 0xff],
+      pickable: true,
+      stroked: true,
+      filled: false,
+      lineWidthScale: 20,
+      lineWidthMinPixels: 2,
+      getLineColor: [255, 0, 0], // 赤色の線
+      getLineWidth: 2,
+    }));
+
+    layerArr.push(new GeoJsonLayer<StopGeoJsonProp>({
       id: 'station',
-      data: stations,
-      pointType: 'circle+text',
-      getText: (f: Feature<Geometry, GeoJsonProp>) => f.properties.name,
-      textCharacterSet: charset,
-    }))
+      data: stops,
+      getText: (f: Feature<Point, StopGeoJsonProp>) => f.properties.stop_name,
+      textCharacterSet: 'auto',
+      getPointRadius: 4,
+      filled: true,
+      getFillColor: [0xff, 0xff, 0xff],
+      getTextColor: [0xff, 0xff, 0xff],
+    }));
+
     return layerArr;
-  }, [stations])
+  }, [stops, routes])
+
+  const loadStops = async () => {
+    const resp = await fetch('/data/stops.json')
+    const json = await resp.json()
+    setStops(json)
+  }
+
+  const loadRoutes = async () => {
+    const resp = await fetch('/data/shapes.json')
+    const json = await resp.json()
+    setRoutes(json)
+  }
 
   useEffect(() => {
-    const load = async () => {
-      const resp = await fetch('/stations.json')
-      const json = await resp.json()
-      setStations(json)
-    }
-    load();
-  })
+    Promise.all([loadStops(), loadRoutes()])
+      .catch(e => console.error("cannot load data:", e));
+  }, []);
 
   return (
     <main className="absolute h-full w-full top-0 left-0">
       <Map
         initialViewState={INITIAL_VIEW_STATE}
-        mapStyle="https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json"
+        //mapStyle="https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json"
+        mapStyle='https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'
         reuseMaps
         id="map"
       >
-        <DeckGLOverlay layers={layers} />
+        <DeckGLOverlay controller layers={layers} />
       </Map>
     </main>
   );
