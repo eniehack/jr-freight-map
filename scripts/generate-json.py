7dbc69bf867622f7f004df5445ce2d3d2131c916
Nakaya <eniehack@outlook.jp>
2025-04-13 18:39:52 +0900

    feat: 路線網のLineStringを生成するscript

--- scripts/generate-json.py
+++ scripts/generate-json.py
@@ -3,24 +3,39 @@ import csv
 import argparse
 from pathlib import Path
 import sys
+from dataclasses import dataclass
+import duckdb
+
+@dataclass
+class Trip:
+    id: str
+    arrival: str | None
+    dept: str | None
+    station_id: str
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--gtfs_dir", type=Path)
+    parser.add_argument("--gtfs_dir", "-i", type=Path)
+    parser.add_argument("--output_dir", "-o", type=Path, default='.')
     args = parser.parse_args()
     geojson_root = {"type": "FeatureCollection", "features": []}
-    with open(args.gtfs_dir / "stops.txt") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            geojson_root["features"].append({
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [float(row["stop_lon"]), float(row["stop_lat"])],
-                },
-                "properties": {
-                    "name": row["stop_name"],
-                    "id": row["stop_id"]
-                }
-            })
-    json.dump(geojson_root, sys.stdout, ensure_ascii=False)
+
+    conn = duckdb.connect()
+    conn.install_extension("spatial")
+    conn.load_extension("spatial")
+    conn.execute("CREATE TABLE shapes_raw AS SELECT shape_id, shape_pt_sequence, st_point(shape_pt_lon, shape_pt_lat) as point FROM read_csv(?, header=true);", [str(args.gtfs_dir / "shapes.txt")])
+    conn.execute("CREATE TABLE shapes AS SELECT shape_id, st_makeline(list(point ORDER BY shape_pt_sequence ASC)) as line FROM shapes_raw GROUP BY shape_id;")
+    shapes = conn.execute("SELECT shape_id, st_asgeojson(line) FROM shapes;").fetchall()
+    for shape in shapes:
+        geom = json.loads(shape[1])
+        geojson = {
+            'type': 'Feature',
+            'geometry': geom,
+            'property': {
+                'shape_id': shape[0]
+            }
+        }
+        geojson_root['features'].append(geojson)
+
+    with open(args.output_dir / 'shapes.json', 'w') as f:
+        json.dump(geojson_root, f)
